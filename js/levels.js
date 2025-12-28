36bce84b42ecb6757faa17955bbfebf70db16f18
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-12-28 11:02:41 +0100

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -4,13 +4,15 @@ const levels = [
     lat: 51.236593,
     lng: 4.413590,
     type: "text",
-    question: "Op welke boot zit je?"
+    question: "Op welke boot zit je?",
+    answer: "batelier"
   },
   {
     lat: 51.236593,
     lng: 4.413590,
     type: "number",
-    question: "Hoeveel letters heeft de partnernaam?"
+    question: "Hoeveel letters heeft de partnernaam?",
+    answer: "3"
   },
   {
     lat: 51.236593,

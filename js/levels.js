4eca760bb3818e9978ca072a1a12941fd455de76
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-12-28 11:21:56 +0100

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -5,19 +5,22 @@ const levels = [
     lng: 4.413590,
     type: "text",
     question: "Op welke boot zit je?",
-    answer: "batelier"
+    answer: "batelier",
+    editable: true
   },
   {
     lat: 51.236593,
     lng: 4.413590,
     type: "number",
     question: "Hoeveel letters heeft de partnernaam?",
-    answer: "3"
+    answer: "3",
+    editable: true
   },
   {
     lat: 51.236593,
     lng: 4.413590,
     type: "photo",
-    question: "Maak een groepsfoto bij het standbeeld"
+    question: "Maak een groepsfoto bij het standbeeld",
+    editable: true
   }
 ];
